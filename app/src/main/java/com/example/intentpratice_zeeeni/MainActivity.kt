7268011dc111981910527b4d5bb7d2ca9d60c81b
Jieun <wldms941114@gmail.com>
2022-11-22 20:20:06 +0900

    Intent이용 -> 다른 화면으로 이동

--- app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.intentpratice_zeeeni
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+//            다른 화면으로 이동(Other Activity)
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+
+
+        }
+
     }
 }
