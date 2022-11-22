cc30201f91f81952d0e4e1a56849dc65be9b2dc1
Jieun <wldms941114@gmail.com>
2022-11-22 23:16:45 +0900

    Dial 액션 예제 코드 구현

--- app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.intentpratice_zeeeni
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +13,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        DIAL 액션 예제
+
+        dialBtn.setOnClickListener {
+
+//            phoneNumEdt에 입력한 전화번호를 받아서 => 해당 번호에 전화 연결
+
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+            val  myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
+
+        }
 
         editNicknameBtn.setOnClickListener {
 
