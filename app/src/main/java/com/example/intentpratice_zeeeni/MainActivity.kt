c5ee2b81517ff006a54342a053aed21775658f8f
Jieun <wldms941114@gmail.com>
2022-11-22 21:38:19 +0900

    닉네임 보여주는 UI추가

--- app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
@@ -10,6 +10,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sendMessageBtn.setOnClickListener {
+
+//            입력한 내용을 변수에 저장.
+
+            val inputMessage = messageEdt.text.toString()
+
+//            비행기티켓 발권
+
+            val myIntent = Intent(this, MessageActivity::class.java)
+
+//            수하물 첨부
+
+            myIntent.putExtra("message", inputMessage)
+
+//            실제 출발
+
+            startActivity(myIntent)
+
+        }
+
         moveToOtherBtn.setOnClickListener {
 
 //            다른 화면으로 이동(Other Activity)
