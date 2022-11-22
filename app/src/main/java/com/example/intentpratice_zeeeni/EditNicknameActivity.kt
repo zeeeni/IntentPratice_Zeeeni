5879556cf056c6992c0aa2e99a3e1dcf1d129518
Jieun <wldms941114@gmail.com>
2022-11-22 22:19:46 +0900

    닉네임 변경을 위한 화면 이동 (startActivityForResult) 활용

--- app/src/main/java/com/example/intentpratice_zeeeni/EditNicknameActivity.kt
+++ app/src/main/java/com/example/intentpratice_zeeeni/EditNicknameActivity.kt
@@ -2,10 +2,14 @@ package com.example.intentpratice_zeeeni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nickname)
+
+        okBtn.setOnClickListener {  }
+
     }
 }
--- app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
+++ app/src/main/java/com/example/intentpratice_zeeeni/MainActivity.kt
@@ -6,9 +6,19 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_FOR_NICKNAME = 1005
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNicknameBtn.setOnClickListener {
+
+            val myIntent = Intent(this, EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQUEST_FOR_NICKNAME)
+
+        }
 
         sendMessageBtn.setOnClickListener {
 
